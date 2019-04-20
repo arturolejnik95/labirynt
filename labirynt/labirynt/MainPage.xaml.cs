3f2aad73f6eb852e4c691c1ccd9c74b5bec9f9b2
arturolejnik95 <arturolejnik95@gmail.com>
2019-04-20 03:53:44 +0200

    Dodaj pliki

---
+++ labirynt/labirynt/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace labirynt
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnLevelValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            double value = args.NewValue;
+            levelLabel.Text = String.Format("{0}", Math.Ceiling(value+1));
+        }
+
+        void OnLivesValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            double value = args.NewValue;
+            livesLabel.Text = String.Format("{0}", Math.Ceiling(value+1));
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            //DO
+        }
+    }
+}
